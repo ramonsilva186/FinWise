9b66982d281aa2937f92536a7d66275f31bfd7d9
ramonsilva186 <ramonsilva186@gmail.com>
2024-12-12 13:22:59 -0300

    Project Created

---
+++ src/test/java/com/ramonsilva/finwise/FinWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonsilva.finwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
